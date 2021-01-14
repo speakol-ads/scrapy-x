fd90e40c5a8d9373fa1b5b7a1da8d4ecd8be0297
Mohamed Al Ashaal <m7medalash3al@gmail.com>
2021-01-14 18:08:50 +0200

    upgraded version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     name='scrapy-x',
     packages=find_packages(),
     install_requires=required,
-    version='1.4',
+    version='1.5',
     author='Mohamed Al Ashaal',
     author_email='m7medalash3al@gmail.com',
     license='Apache License V2.0',
